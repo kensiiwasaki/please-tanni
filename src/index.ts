d49dfe84c45b6bca38fd4e189320afcee5bccc9a
kensiiwasaki <kensiiwasaki@gmail.com>
2024-01-31 02:15:36 +0900

    リクエストとレスポンス

--- src/index.ts
+++ src/index.ts
@@ -12,4 +12,12 @@ app.get("/api/hello", (c) => {
     message: "Hello Hono!",
   });
 });
+
+app.get("/posts/:id", (c) => {
+  const page = c.req.query("page");
+  const id = c.req.param("id");
+  c.header("X-Message", "Hi!");
+  return c.text(`You want see ${page} of ${id}`);
+});
+
 export default app;
