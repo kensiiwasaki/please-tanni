ff6e06658092f2fbd5bf9f077fa10cda87b9e67f
kensiiwasaki <kensiiwasaki@gmail.com>
2024-02-07 23:59:43 +0900

    フォーマット

--- src/schema.ts
+++ src/schema.ts
@@ -6,7 +6,7 @@ const users = sqliteTable("users", {
   name: text("name").notNull(),
   email: text("email").unique().notNull(),
   createdAt: integer("created_at", { mode: "timestamp" }).default(
-    sql`(strftime('%s', 'now'))`
+    sql`(strftime('%s', 'now'))`,
   ),
 });
 
