61d645c306e16d1758ebf3f98398ce7c312d52be
kensiiwasaki <kensiiwasaki@gmail.com>
2024-02-07 21:37:42 +0900

    drizzleのschema定義

---
+++ src/schema.ts
@@ -0,0 +1,13 @@
+import { sql } from "drizzle-orm";
+import { integer, text, sqliteTable } from "drizzle-orm/sqlite-core";
+
+const users = sqliteTable("users", {
+  id: integer("id", { mode: "number" }).primaryKey({ autoIncrement: true }),
+  name: text("name").notNull(),
+  email: text("email").unique().notNull(),
+  createdAt: integer("created_at", { mode: "timestamp" }).default(
+    sql`(strftime('%s', 'now'))`
+  ),
+});
+
+export default users;
